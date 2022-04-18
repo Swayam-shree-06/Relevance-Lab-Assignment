69f07d551e5f4ae3fac7e108951c42401caf948c
Swayam-shree-06 <97376721+Swayam-shree-06@users.noreply.github.com>
2022-04-19 05:10:02 +0530

    Add files via upload

---
+++ src/AddBlog.js
@@ -0,0 +1,141 @@
+// formik for form validation
+import {  useFormik } from "formik";
+
+// yup is used for validate condition
+import * as yup from "yup";
+
+import React from "react";
+import { useHistory } from "react-router-dom";
+// import { useState } from "react";
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import "./CSS/AddBlog.css"
+
+const formValidationSchema = yup.object({
+  title:yup
+  .string()
+  .min(4)
+  .required(),
+  
+  description:yup
+  .string()
+  .min(4)
+  .required(),
+  
+  picture:yup
+  .string()
+  .min(4)
+  .required(),
+  
+  text:yup
+  .string()
+  .min(4)
+  .required(),
+
+});
+
+export function AddBlog() {
+ 
+  
+  // Bulit in functions in Formik
+  const {handleSubmit,values,handleChange,handleBlur,errors,touched}=
+    useFormik({
+      // assign initial values for add data
+        initialValues:{title:"",description:"",picture:"",text:""},
+        validationSchema:formValidationSchema,
+        onSubmit:(values)=>{
+            console.log("onSubmit", values);
+        }
+    })
+  const history = useHistory();
+
+  return (
+      <div className="new-Blog-list">
+      <form onSubmit={handleSubmit}>
+     
+     <div className="addBlogDivision">
+     <TextField
+            // based on id and name only formik know what is change.
+          id="title"
+          name="title"
+          value={values.title}
+          // handle the change value
+          onChange={handleChange}
+          // setting touch varible
+          onBlur={handleBlur}
+          type="title"
+          placeholder="Title"
+          label="Title" 
+          variant="outlined"  /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+{errors.title && touched.title ? errors.title : ""}
+
+        <TextField
+         id="description"
+         name="description"
+         value={values.description}
+         // handle the change value
+         onChange={handleChange}
+         // setting touch varible
+         onBlur={handleBlur}
+         type="description"
+         placeholder="Description"
+         label="Description" 
+         variant="outlined"  />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+{errors.description && touched.description ? errors.description : ""}
+
+        <TextField
+          id="picture"
+          name="picture"
+          value={values.picture} 
+          // handle the change value
+          onChange={handleChange}
+           // setting touch varible
+          onBlur={handleBlur}
+          type="picture"
+          placeholder="Picture"
+          label="Picture" 
+          variant="outlined"  />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ {errors.picture && touched.picture ? errors.picture : ""}
+ 
+        <TextField
+          id="text"
+          name="text"
+          value={values.text} 
+          // handle the change value
+          onChange={handleChange}
+           // setting touch varible
+          onBlur={handleBlur}
+          type="text"
+          placeholder="Text"
+          label="Text" 
+          variant="outlined"  />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ {errors.text && touched.text ? errors.text : ""}
+     </div>
+ 
+     
+      <div className="blogButton">
+      <Button  style={{backgroundColor:"#63dee0"}}
+        onClick={() => {
+          const newBlog = {
+            title: values.title,
+            description: values.description,
+            picture: values.picture,
+            text: values.text
+          };
+          fetch("https://618fa736f6bf4500174849a7.mockapi.io/blog/",{
+            method:"POST",
+            body:JSON.stringify(newBlog),
+            headers:{
+              "Content-type":"application/json"
+            }
+              }).then(()=>history.push("/blog/home"))
+                }}
+        variant="outlined">Add Blog</Button>
+      </div>
+ </form>
+    </div>
+   
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+
+import "./index.css";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Switch, Route, useHistory } from "react-router-dom";
+import { Home } from "./Home";
+import { Register } from "./Register";
+import { Login } from "./Login";
+import { EditBlog } from "./EditBlog";
+
+import "./CSS/App.css";
+import logo from './IMAGE/download.png';
+
+
+export default function App() {
+  const history = useHistory();
+
+  return (
+
+    <div className="App">
+      <nav class="navbar navbar-expand-lg navbar-light bg-success">
+
+        <img src={logo} width="60" height="40" alt="" />
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#demo-navbar"
+          aria-controls="demo-navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="demo-navbar">
+
+            <a class="nav-link text-light" onClick={() => history.push("/blog/home")}>Home <span class="sr-only">(current)</span></a>
+
+            <a class="nav-link text-light" onClick={() => history.push("/blog/signup")}>SIGN UP</a>
+
+            <a class="nav-link text-light" onClick={() => history.push("/blog/signin")}>SIGN IN</a>
+
+
+            <a class="nav-link text-light" onClick={() => history.push("/")}>LOGOUT</a>
+    
+
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"  id="serchBar"/>
+          </form>
+          <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" id="buttonWork">Search</button>
+        </div>
+      </nav>
+
+      <section class="header">
+    <h1>Let's BLOG</h1>
+    <p>Enjoy writing your blogs here..</p>
+    {/* <a class="btn-bgstroke">Call To Action</a> */}
+</section>
+
+      <Switch>
+        <Route path="/blog/edit/:id">
+          <h1>Edit Blog</h1>
+          <EditBlog />
+        </Route>
+
+        <Route path="/blog/signup">
+          <Register />
+        </Route>
+
+        <Route path="/blog/signin">
+          <Login />
+        </Route>
+
+
+
+
+
+        <Route path="/blog/home">
+          <Home />
+        </Route>
+      </Switch>
+
+
+
+      <section class="footer">
+      <div class="social">
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-snapchat"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+      </div>
+
+      <ul class="list">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Terms</a>
+        </li>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+      </ul>
+      <p class="copyright"><b>@RelevanceLab Pvt.Ltd</b></p>
+    </section>
+    </div>
+
+
+
+  );
+}
+
+
+
+
+
+
